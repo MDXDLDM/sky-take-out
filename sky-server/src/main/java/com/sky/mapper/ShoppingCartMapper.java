3063baf8f4a51036132eb40a9b54e7d94cc99c5e
MDXDLDM <2428416768@qq.com>
2025-06-07 15:45:37 +0800

    添加购物车功能开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,32 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    /**
+     *
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     *
+     * @param shoppingCart1
+     */
+    @Update("update shopping_cart set number=#{number} where id=#{id}")
+    void update(ShoppingCart shoppingCart1);
+
+    /**
+     *
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart(name, image, user_id, dish_id, setmeal_id, dish_flavor, amount, create_time) VALUES " +
+            "(#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{amount},#{createTime})")
+    void insert(ShoppingCart shoppingCart);
+}
---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,11 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+
+public interface ShoppingCartService {
+    /**
+     *
+     * @param shoppingCartDTO
+     */
+    void add(ShoppingCartDTO shoppingCartDTO);
+}
